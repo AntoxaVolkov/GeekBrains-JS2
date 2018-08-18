e9fe7325542fe456ad864da958a11cdb48320dc9
AntoxaVolkov <m-a-r-v-i-n@mail.ru>
2018-08-19 00:46:54 +0300

    Третий урок по JS2 - готов

--- Lesson_3/main.js
+++ Lesson_3/main.js
@@ -3,14 +3,23 @@ document.addEventListener('DOMContentLoaded', init);
 function init() {
     "use strict";
     const btnVFirst = document.querySelector('#replaceFirst');
+    const btnVSecond = document.querySelector('#replaceSecond');
     const textarea = document.querySelector('#text');
     const output = document.querySelector('.output');
     
     btnVFirst.addEventListener('click', replaceFirst);
+
+    btnVSecond.addEventListener('click', replaceSecond);
    
+    
     function replaceFirst(ev) {
         const reg = /\B'(.+?)'\B/gmi
         output.innerText = textarea.value.replace(reg, '"$1"');
+    }
+
+    function replaceSecond(ev) {
+        const reg = /([^a-zA-Zа-яА-ЯёЁ]|^)'(.+?)'([^a-zA-Zа-яА-ЯёЁ]|$)/gmi
+        output.innerText = textarea.value.replace(reg, '$1"$2"$3');
     }
 }
 
